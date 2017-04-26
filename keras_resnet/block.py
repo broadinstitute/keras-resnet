47acdda033b63255b6f5e95bb7728a622e417b2a
Allen Goodman <allen.goodman@icloud.com>
2017-04-26 12:07:02 -0400

    fix shortcut padding

--- keras_resnet/block.py
+++ keras_resnet/block.py
@@ -98,12 +98,12 @@ def shortcut(a, b):
         y = int(round(a_shape[2] / b_shape[2]))
 
         if x > 1 or y > 1 or not a_shape[3] == b_shape[3]:
-            a = keras.layers.Conv2D(b_shape[3], (1, 1), strides=(x, y))(a)
+            a = keras.layers.Conv2D(b_shape[3], (1, 1), strides=(x, y), padding="same")(a)
     else:
         x = int(round(a_shape[2] / b_shape[2]))
         y = int(round(a_shape[3] / b_shape[3]))
 
         if x > 1 or y > 1 or not a_shape[3] == b_shape[3]:
-            a = keras.layers.Conv2D(b_shape[3], (1, 1), strides=(x, y))(a)
+            a = keras.layers.Conv2D(b_shape[3], (1, 1), strides=(x, y), padding="same")(a)
 
     return keras.layers.add([a, b])
