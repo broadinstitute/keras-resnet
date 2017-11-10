0b0c5fae268a673cc065913cef65277a3818e102
Hans Gaiser <j.c.gaiser@delftrobotics.com>
2017-11-10 13:02:36 +0100

    Fix time distributed typo.

--- keras_resnet/models/_time_distributed_2d.py
+++ keras_resnet/models/_time_distributed_2d.py
@@ -66,7 +66,7 @@ def TimeDistributedResNet(inputs, blocks, block, include_top=True, classes=1000,
     x = keras.layers.TimeDistributed(keras.layers.Conv2D(64, (7, 7), strides=(2, 2), use_bias=False), name="conv1")(x)
     x = keras.layers.TimeDistributed(keras_resnet.layers.BatchNormalization(axis=axis, epsilon=1e-5, freeze=freeze_bn), name="bn_conv1")(x)
     x = keras.layers.TimeDistributed(keras.layers.Activation("relu"), name="conv1_relu")(x)
-    x = keras.layers.TimeDistributed(keras.layers.MaxPooling2D((3, 3), strides=(2, 2)), padding="same", name="pool1")(x)
+    x = keras.layers.TimeDistributed(keras.layers.MaxPooling2D((3, 3), strides=(2, 2), padding="same"), name="pool1")(x)
 
     features = 64
 
