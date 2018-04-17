08ca7d162dea872c1225680dca3936bf1e3ed01b
Allen Goodman <allen.goodman@icloud.com>
2018-04-17 11:22:25 -0400

    tests

--- tests/layers/test_group_normalization.py
+++ tests/layers/test_group_normalization.py
@@ -1,18 +1,18 @@
-import pytest
-import numpy as np
-from numpy.testing import assert_allclose
-
-from keras.layers import Input
-from keras.models import Sequential, Model
-import keras_resnet.layers
-import keras.utils.test_utils
 import keras.backend
+import keras.layers
+import keras.models
 import keras.regularizers
+import keras.utils.test_utils
+import numpy
+import numpy.testing
+import pytest
 
-input_1 = np.arange(10)
-input_2 = np.zeros(10)
-input_3 = np.ones((10))
-input_shapes = [np.ones((10, 10)), np.ones((10, 10, 10))]
+import keras_resnet.layers
+
+input_1 = numpy.arange(10)
+input_2 = numpy.zeros(10)
+input_3 = numpy.ones(10)
+input_shapes = [numpy.ones((10, 10)), numpy.ones((10, 10, 10))]
 
 
 @keras.utils.test_utils.keras_test
@@ -63,137 +63,105 @@ def test_group_normalization():
 
 @keras.utils.test_utils.keras_test
 def test_group_normalization_1d():
-    model = Sequential()
+    model = keras.models.Sequential()
     norm = keras_resnet.layers.GroupNormalization(input_shape=(10,), groups=2)
     model.add(norm)
     model.compile(loss="mse", optimizer="rmsprop")
 
     # centered on 5.0, variance 10.0
-    x = np.random.normal(loc=5.0, scale=10.0, size=(1000, 10))
+    x = numpy.random.normal(loc=5.0, scale=10.0, size=(1000, 10))
     model.fit(x, x, epochs=5, verbose=0)
     out = model.predict(x)
     out -= keras.backend.eval(norm.beta)
     out /= keras.backend.eval(norm.gamma)
 
-    assert_allclose(out.mean(), 0.0, atol=1e-1)
-    assert_allclose(out.std(), 1.0, atol=1e-1)
+    numpy.testing.assert_allclose(out.mean(), 0.0, atol=1e-1)
+    numpy.testing.assert_allclose(out.std(), 1.0, atol=1e-1)
 
 
 @keras.utils.test_utils.keras_test
 def test_group_normalization_2d():
-    model = Sequential()
+    model = keras.models.Sequential()
     norm = keras_resnet.layers.GroupNormalization(axis=1, input_shape=(10, 6), groups=2)
     model.add(norm)
     model.compile(loss="mse", optimizer="rmsprop")
 
     # centered on 5.0, variance 10.0
-    x = np.random.normal(loc=5.0, scale=10.0, size=(1000, 10, 6))
+    x = numpy.random.normal(loc=5.0, scale=10.0, size=(1000, 10, 6))
     model.fit(x, x, epochs=5, verbose=0)
     out = model.predict(x)
-    out -= np.reshape(keras.backend.eval(norm.beta), (1, 10, 1))
-    out /= np.reshape(keras.backend.eval(norm.gamma), (1, 10, 1))
+    out -= numpy.reshape(keras.backend.eval(norm.beta), (1, 10, 1))
+    out /= numpy.reshape(keras.backend.eval(norm.gamma), (1, 10, 1))
 
-    assert_allclose(out.mean(axis=(0, 2)), 0.0, atol=1.1e-1)
-    assert_allclose(out.std(axis=(0, 2)), 1.0, atol=1.1e-1)
+    numpy.testing.assert_allclose(out.mean(axis=(0, 2)), 0.0, atol=1.1e-1)
+    numpy.testing.assert_allclose(out.std(axis=(0, 2)), 1.0, atol=1.1e-1)
 
 
 @keras.utils.test_utils.keras_test
-def test_groupnorm_correctness_2d_different_groups():
+def test_group_normalization_2d_different_groups():
     norm1 = keras_resnet.layers.GroupNormalization(axis=1, input_shape=(10, 6), groups=2)
     norm2 = keras_resnet.layers.GroupNormalization(axis=1, input_shape=(10, 6), groups=1)
     norm3 = keras_resnet.layers.GroupNormalization(axis=1, input_shape=(10, 6), groups=10)
 
-    model = Sequential()
+    model = keras.models.Sequential()
     model.add(norm1)
     model.compile(loss="mse", optimizer="rmsprop")
 
     # centered on 5.0, variance 10.0
-    x = np.random.normal(loc=5.0, scale=10.0, size=(1000, 10, 6))
+    x = numpy.random.normal(loc=5.0, scale=10.0, size=(1000, 10, 6))
     model.fit(x, x, epochs=5, verbose=0)
     out = model.predict(x)
-    out -= np.reshape(keras.backend.eval(norm1.beta), (1, 10, 1))
-    out /= np.reshape(keras.backend.eval(norm1.gamma), (1, 10, 1))
+    out -= numpy.reshape(keras.backend.eval(norm1.beta), (1, 10, 1))
+    out /= numpy.reshape(keras.backend.eval(norm1.gamma), (1, 10, 1))
 
-    assert_allclose(out.mean(axis=(0, 2)), 0.0, atol=1.1e-1)
-    assert_allclose(out.std(axis=(0, 2)), 1.0, atol=1.1e-1)
+    numpy.testing.assert_allclose(out.mean(axis=(0, 2)), 0.0, atol=1.1e-1)
+    numpy.testing.assert_allclose(out.std(axis=(0, 2)), 1.0, atol=1.1e-1)
 
-    model = Sequential()
+    model = keras.models.Sequential()
     model.add(norm2)
     model.compile(loss="mse", optimizer="rmsprop")
 
     # centered on 5.0, variance 10.0
-    x = np.random.normal(loc=5.0, scale=10.0, size=(1000, 10, 6))
+    x = numpy.random.normal(loc=5.0, scale=10.0, size=(1000, 10, 6))
     model.fit(x, x, epochs=5, verbose=0)
     out = model.predict(x)
-    out -= np.reshape(keras.backend.eval(norm2.beta), (1, 10, 1))
-    out /= np.reshape(keras.backend.eval(norm2.gamma), (1, 10, 1))
+    out -= numpy.reshape(keras.backend.eval(norm2.beta), (1, 10, 1))
+    out /= numpy.reshape(keras.backend.eval(norm2.gamma), (1, 10, 1))
 
-    assert_allclose(out.mean(axis=(0, 2)), 0.0, atol=1.1e-1)
-    assert_allclose(out.std(axis=(0, 2)), 1.0, atol=1.1e-1)
+    numpy.testing.assert_allclose(out.mean(axis=(0, 2)), 0.0, atol=1.1e-1)
+    numpy.testing.assert_allclose(out.std(axis=(0, 2)), 1.0, atol=1.1e-1)
 
-    model = Sequential()
+    model = keras.models.Sequential()
     model.add(norm3)
     model.compile(loss="mse", optimizer="rmsprop")
 
     # centered on 5.0, variance 10.0
-    x = np.random.normal(loc=5.0, scale=10.0, size=(1000, 10, 6))
+    x = numpy.random.normal(loc=5.0, scale=10.0, size=(1000, 10, 6))
     model.fit(x, x, epochs=5, verbose=0)
     out = model.predict(x)
-    out -= np.reshape(keras.backend.eval(norm3.beta), (1, 10, 1))
-    out /= np.reshape(keras.backend.eval(norm3.gamma), (1, 10, 1))
+    out -= numpy.reshape(keras.backend.eval(norm3.beta), (1, 10, 1))
+    out /= numpy.reshape(keras.backend.eval(norm3.gamma), (1, 10, 1))
 
-    assert_allclose(out.mean(axis=(0, 2)), 0.0, atol=1.1e-1)
-    assert_allclose(out.std(axis=(0, 2)), 1.0, atol=1.1e-1)
-
-
-@keras.utils.test_utils.keras_test
-def test_groupnorm_mode_twice():
-    # This is a regression test for issue #4881 with the old
-    # batch normalization functions in the Theano keras.backend.
-    model = Sequential()
-    model.add(keras_resnet.layers.GroupNormalization(input_shape=(10, 5, 5), axis=1, groups=2))
-    model.add(keras_resnet.layers.GroupNormalization(input_shape=(10, 5, 5), axis=1, groups=2))
-    model.compile(loss="mse", optimizer="sgd")
-
-    x = np.random.normal(loc=5.0, scale=10.0, size=(20, 10, 5, 5))
-    model.fit(x, x, epochs=1, verbose=0)
-    model.predict(x)
+    numpy.testing.assert_allclose(out.mean(axis=(0, 2)), 0.0, atol=1.1e-1)
+    numpy.testing.assert_allclose(out.std(axis=(0, 2)), 1.0, atol=1.1e-1)
 
 
 @keras.utils.test_utils.keras_test
-def test_groupnorm_convnet():
-    model = Sequential()
+def test_group_normalization_convolution():
+    model = keras.models.Sequential()
     norm = keras_resnet.layers.GroupNormalization(axis=1, input_shape=(3, 4, 4), groups=3)
     model.add(norm)
     model.compile(loss="mse", optimizer="sgd")
 
     # centered on 5.0, variance 10.0
-    x = np.random.normal(loc=5.0, scale=10.0, size=(1000, 3, 4, 4))
+    x = numpy.random.normal(loc=5.0, scale=10.0, size=(1000, 3, 4, 4))
     model.fit(x, x, epochs=4, verbose=0)
     out = model.predict(x)
-    out -= np.reshape(keras.backend.eval(norm.beta), (1, 3, 1, 1))
-    out /= np.reshape(keras.backend.eval(norm.gamma), (1, 3, 1, 1))
+    out -= numpy.reshape(keras.backend.eval(norm.beta), (1, 3, 1, 1))
+    out /= numpy.reshape(keras.backend.eval(norm.gamma), (1, 3, 1, 1))
 
-    assert_allclose(np.mean(out, axis=(0, 2, 3)), 0.0, atol=1e-1)
-    assert_allclose(np.std(out, axis=(0, 2, 3)), 1.0, atol=1e-1)
-
-
-@keras.utils.test_utils.keras_test
-@pytest.mark.skipif((keras.backend.backend() == "theano"), reason="Bug with theano backend")
-def test_groupnorm_convnet_no_center_no_scale():
-    model = Sequential()
-    norm = keras_resnet.layers.GroupNormalization(axis=-1, center=False, scale=False,
-                                            input_shape=(3, 4, 4), groups=2)
-    model.add(norm)
-    model.compile(loss="mse", optimizer="sgd")
-
-    # centered on 5.0, variance 10.0
-    x = np.random.normal(loc=5.0, scale=10.0, size=(1000, 3, 4, 4))
-    model.fit(x, x, epochs=4, verbose=0)
-    out = model.predict(x)
-
-    assert_allclose(np.mean(out, axis=(0, 2, 3)), 0.0, atol=1e-1)
-    assert_allclose(np.std(out, axis=(0, 2, 3)), 1.0, atol=1e-1)
+    numpy.testing.assert_allclose(numpy.mean(out, axis=(0, 2, 3)), 0.0, atol=1e-1)
+    numpy.testing.assert_allclose(numpy.std(out, axis=(0, 2, 3)), 1.0, atol=1e-1)
 
 
 @keras.utils.test_utils.keras_test
@@ -203,36 +171,36 @@ def test_group_normalization_shared():
     """
     # Test single layer reuse
     bn = keras_resnet.layers.GroupNormalization(input_shape=(10,), groups=2)
-    x1 = Input(shape=(10,))
+    x1 = keras.layers.Input(shape=(10,))
     bn(x1)
 
-    x2 = Input(shape=(10,))
+    x2 = keras.layers.Input(shape=(10,))
     y2 = bn(x2)
 
-    x = np.random.normal(loc=5.0, scale=10.0, size=(2, 10))
-    model = Model(x2, y2)
+    x = numpy.random.normal(loc=5.0, scale=10.0, size=(2, 10))
+    model = keras.models.Model(x2, y2)
     assert len(model.updates) == 0
     model.compile("sgd", "mse")
     model.train_on_batch(x, x)
 
     # Test model-level reuse
-    x3 = Input(shape=(10,))
+    x3 = keras.layers.Input(shape=(10,))
     y3 = model(x3)
-    new_model = Model(x3, y3)
+    new_model = keras.models.Model(x3, y3)
     assert len(model.updates) == 0
     new_model.compile("sgd", "mse")
     new_model.train_on_batch(x, x)
 
 
 @keras.utils.test_utils.keras_test
-def test_that_trainable_disables_updates():
-    val_a = np.random.random((10, 4))
-    val_out = np.random.random((10, 4))
+def test_group_normalization_trainable():
+    val_a = numpy.random.random((10, 4))
+    val_out = numpy.random.random((10, 4))
 
-    a = Input(shape=(4,))
+    a = keras.layers.Input(shape=(4,))
     layer = keras_resnet.layers.GroupNormalization(input_shape=(4,), groups=2)
     b = layer(a)
-    model = Model(a, b)
+    model = keras.models.Model(a, b)
 
     model.trainable = False
     assert len(model.updates) == 0
@@ -243,7 +211,7 @@ def test_that_trainable_disables_updates():
     x1 = model.predict(val_a)
     model.train_on_batch(val_a, val_out)
     x2 = model.predict(val_a)
-    assert_allclose(x1, x2, atol=1e-7)
+    numpy.testing.assert_allclose(x1, x2, atol=1e-7)
 
     model.trainable = True
     model.compile("sgd", "mse")
@@ -251,7 +219,7 @@ def test_that_trainable_disables_updates():
 
     model.train_on_batch(val_a, val_out)
     x2 = model.predict(val_a)
-    assert np.abs(np.sum(x1 - x2)) > 1e-5
+    assert numpy.abs(numpy.sum(x1 - x2)) > 1e-5
 
     layer.trainable = False
     model.compile("sgd", "mse")
@@ -260,4 +228,4 @@ def test_that_trainable_disables_updates():
     x1 = model.predict(val_a)
     model.train_on_batch(val_a, val_out)
     x2 = model.predict(val_a)
-    assert_allclose(x1, x2, atol=1e-7)
+    numpy.testing.assert_allclose(x1, x2, atol=1e-7)
