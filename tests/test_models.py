1a1a773710ebaa0ea20c772e2ada51d5b4767dbf
Allen Goodman <allen.goodman@icloud.com>
2018-11-28 16:54:50 -0500

    updated tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -3,41 +3,41 @@ import keras_resnet.models
 
 class TestResNet18:
     def test_constructor(self, x):
-        model = keras_resnet.models.ResNet18(x)
+        model = keras_resnet.models.ResNet2D18(x)
 
         assert len(model.layers) == 88
 
 
 class TestResNet34:
     def test_constructor(self, x):
-        model = keras_resnet.models.ResNet34(x)
+        model = keras_resnet.models.ResNet2D34(x)
 
         assert len(model.layers) == 160
 
 
 class TestResNet50:
     def test_constructor(self, x):
-        model = keras_resnet.models.ResNet50(x)
+        model = keras_resnet.models.ResNet2D50(x)
 
         assert len(model.layers) == 192
 
 
 class TestResNet101:
     def test_constructor(self, x):
-        model = keras_resnet.models.ResNet101(x)
+        model = keras_resnet.models.ResNet2D101(x)
 
         assert len(model.layers) == 379
 
 
 class TestResNet152:
     def test_constructor(self, x):
-        model = keras_resnet.models.ResNet152(x)
+        model = keras_resnet.models.ResNet2D152(x)
 
         assert len(model.layers) == 566
 
 
 class TestResNet200:
     def test_constructor(self, x):
-        model = keras_resnet.models.ResNet200(x)
+        model = keras_resnet.models.ResNet2D200(x)
 
         assert len(model.layers) == 742
