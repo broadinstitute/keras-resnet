68e1f7ba4a7503de16c98aa46ce2ebf9c3abca26
Allen Goodman <allen.goodman@icloud.com>
2018-11-28 16:30:57 -0500

    1-and-3D models

--- keras_resnet/models/__init__.py
+++ keras_resnet/models/__init__.py
@@ -7,6 +7,16 @@ keras_resnet.models
 This module implements popular residual models.
 """
 
+from ._1d import (
+    ResNet1D,
+    ResNet1D18,
+    ResNet1D34,
+    ResNet1D50,
+    ResNet1D101,
+    ResNet1D152,
+    ResNet1D200
+)
+
 from ._2d import (
     ResNet,
     ResNet18,
@@ -15,6 +25,16 @@ from ._2d import (
     ResNet101,
     ResNet152,
     ResNet200
+)
+
+from ._3d import (
+    ResNet3D,
+    ResNet3D18,
+    ResNet3D34,
+    ResNet3D50,
+    ResNet3D101,
+    ResNet3D152,
+    ResNet3D200
 )
 
 from ._time_distributed_2d import (
