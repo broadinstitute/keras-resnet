0190ea175bcf1686688be0f2e3043549eddce60c
Matthieu Broisin <mbroisin@broadinstitute.org>
2018-11-29 15:25:06 -0500

    first commit test

--- keras_resnet/models/_2d.py
+++ keras_resnet/models/_2d.py
@@ -15,6 +15,8 @@ import keras.regularizers
 import keras_resnet.blocks
 import keras_resnet.layers
 
+import tensorflow.contrib.layers
+
 
 class ResNet2D(keras.Model):
     """
@@ -74,7 +76,10 @@ class ResNet2D(keras.Model):
             numerical_names = [True] * len(blocks)
 
         x = keras.layers.Conv2D(64, (7, 7), strides=(2, 2), use_bias=False, name="conv1", padding="same")(inputs)
-        x = keras_resnet.layers.BatchNormalization(axis=axis, epsilon=1e-5, freeze=freeze_bn, name="bn_conv1")(x)
+
+        x = keras.layers.Lambda(lambda x_in: tensorflow.contrib.layers.group_norm(x_in, groups=32, channels_axis=axis))(x)
+
+        # x = keras_resnet.layers.BatchNormalization(axis=axis, epsilon=1e-5, freeze=freeze_bn, name="bn_conv1")(x)
         x = keras.layers.Activation("relu", name="conv1_relu")(x)
         x = keras.layers.MaxPooling2D((3, 3), strides=(2, 2), padding="same", name="pool1")(x)
 
