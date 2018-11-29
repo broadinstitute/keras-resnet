8c264862615f69839760c1c2e7a9212c7365320d
Matthieu Broisin <mbroisin@broadinstitute.org>
2018-11-29 16:15:04 -0500

    keras resnet test

--- keras_resnet/models/_2d.py
+++ keras_resnet/models/_2d.py
@@ -77,9 +77,9 @@ class ResNet2D(keras.Model):
 
         x = keras.layers.Conv2D(64, (7, 7), strides=(2, 2), use_bias=False, name="conv1", padding="same")(inputs)
 
-        x = keras.layers.Lambda(lambda x_in: tensorflow.contrib.layers.group_norm(x_in, groups=32, channels_axis=axis))(x)
+        # x = keras.layers.Lambda(lambda x_in: tensorflow.contrib.layers.group_norm(x_in, groups=32, channels_axis=axis))(x)
 
-        # x = keras_resnet.layers.BatchNormalization(axis=axis, epsilon=1e-5, freeze=freeze_bn, name="bn_conv1")(x)
+        x = keras_resnet.layers.BatchNormalization(axis=axis, epsilon=1e-5, freeze=freeze_bn, name="bn_conv1")(x)
         x = keras.layers.Activation("relu", name="conv1_relu")(x)
         x = keras.layers.MaxPooling2D((3, 3), strides=(2, 2), padding="same", name="pool1")(x)
 
