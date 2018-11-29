5005c374fd58b9cea53c0fa640ddb159e0f4bb0f
Allen Goodman <allen.goodman@icloud.com>
2018-11-29 14:06:51 -0500

    removing ZeroPadding

--- keras_resnet/models/_2d.py
+++ keras_resnet/models/_2d.py
@@ -73,8 +73,7 @@ class ResNet2D(keras.Model):
         if numerical_names is None:
             numerical_names = [True] * len(blocks)
 
-        x = keras.layers.ZeroPadding2D(padding=3, name="padding_conv1")(inputs)
-        x = keras.layers.Conv2D(64, (7, 7), strides=(2, 2), use_bias=False, name="conv1")(x)
+        x = keras.layers.Conv2D(64, (7, 7), strides=(2, 2), use_bias=False, name="conv1", padding="same")(inputs)
         x = keras_resnet.layers.BatchNormalization(axis=axis, epsilon=1e-5, freeze=freeze_bn, name="bn_conv1")(x)
         x = keras.layers.Activation("relu", name="conv1_relu")(x)
         x = keras.layers.MaxPooling2D((3, 3), strides=(2, 2), padding="same", name="pool1")(x)
--- tests/test_models.py
+++ tests/test_models.py
@@ -5,39 +5,39 @@ class TestResNet18:
     def test_constructor(self, x):
         model = keras_resnet.models.ResNet2D18(x)
 
-        assert len(model.layers) == 88
+        assert len(model.layers) == 87
 
 
 class TestResNet34:
     def test_constructor(self, x):
         model = keras_resnet.models.ResNet2D34(x)
 
-        assert len(model.layers) == 160
+        assert len(model.layers) == 159
 
 
 class TestResNet50:
     def test_constructor(self, x):
         model = keras_resnet.models.ResNet2D50(x)
 
-        assert len(model.layers) == 192
+        assert len(model.layers) == 191
 
 
 class TestResNet101:
     def test_constructor(self, x):
         model = keras_resnet.models.ResNet2D101(x)
 
-        assert len(model.layers) == 379
+        assert len(model.layers) == 378
 
 
 class TestResNet152:
     def test_constructor(self, x):
         model = keras_resnet.models.ResNet2D152(x)
 
-        assert len(model.layers) == 566
+        assert len(model.layers) == 565
 
 
 class TestResNet200:
     def test_constructor(self, x):
         model = keras_resnet.models.ResNet2D200(x)
 
-        assert len(model.layers) == 742
+        assert len(model.layers) == 741
